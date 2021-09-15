fd6feaa56034a777cc05dd11a50b2cc5270834c5
lenhatnam82 <lenhatnam10b5@gmail.com>
2021-09-15 18:56:08 +0700

    custom 1

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import modelBlog
+from django.core.paginator import Paginator
+
+
+
 def index(request):
     listBlog = modelBlog.objects.all()
-    return render(request,"home/index.html",{"listBlog":listBlog})
+    paginator = Paginator(listBlog, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,"home/index.html",{"listBlog":page_obj})
